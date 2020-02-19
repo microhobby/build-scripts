5a100a1ec59597f511e6f5c2f9e64cf6055ebebc
Matheus Castello <matheus@castello.eng.br>
2020-02-18 23:36:30 -0300

    build: armhf: labrador: Add dtb prefix and ramdisk path
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- build-labrador-v2.sh
+++ build-labrador-v2.sh
@@ -9,8 +9,10 @@
 export arg=""
 export path="/media/castello/SYSTEM"
 export path_boot="/media/castello/BOOT"
+export path_ramdisk="/media/castello/ramdisco"
 export artifacts="../artifacts/labrador/"
 export defconfig="../../../../configs/labrador/labrador_defconfig"
+export dtb_prefix="owl"
 export jobs=12
 export kernel_src="../linux"
 
